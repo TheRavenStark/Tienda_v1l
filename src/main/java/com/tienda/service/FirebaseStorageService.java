4a95b8f9dd23846f4c4531d80d57560454cb637a
TheRavenStark <113565611+TheRavenStark@users.noreply.github.com>
2024-02-19 20:59:50 -0600

    Semana 6
    Trabajo en clase: fragmentos, firebase, dependecy, listado

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,22 @@
+package com.tienda.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-v1-lu.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "Tienda";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-v1-lu-firebase-adminsdk-mej3h-1f85a48c23";
+}
